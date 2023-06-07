15b1f99c9f777547fb556a5eb710805a66cdeaab
Daniel <angelicodaniel93@gmail.com>
2023-06-07 13:15:23 +0200

    first commit

---
+++ src/org/lessons/java/Book.java
@@ -0,0 +1,15 @@
+package org.lessons.java;
+
+class Book {
+    private String title;
+    private int numPages;
+    private String author;
+    private String publisher;
+
+    public Book(String title, int numPages, String author, String publisher) {
+        this.title = title;
+        this.numPages = numPages;
+        this.author = author;
+        this.publisher = publisher;
+    }
+}
